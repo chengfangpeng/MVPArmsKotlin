210fb9d50299083bcd92a2f3cafb0b8c3eaa5b24
chengfangpeng <chengfangpeng@foxmail.com>
2018-01-20 18:37:49 +0800

    initial commit

---
+++ arms-kotlin/src/main/java/com/arms/kotlin/base/App.kt
@@ -0,0 +1,13 @@
+package com.arms.kotlin.base
+
+/**
+ * @title
+ * @description
+ * @company 北京奔流网络信息技术有线公司
+ * @author cfp
+ * @version 1.0
+ * @created 2018/01/20
+ * @changeRecord [修改记录] <br/>
+ */
+interface App {
+}
